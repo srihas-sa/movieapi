eae37f14bb3fd146d3d26ae5ecd4912d2b58c1fe
srihas-sa <edupugantisrihas2003@gmail.com>
2023-05-01 10:10:26 +0000

    commit three

--- app.js
+++ app.js
@@ -30,13 +30,32 @@ app.get("/movies/", async (request, response) => {
   const { bookId } = request.params;
   const getMovieQuery = `
     SELECT
-      *
+      movie_name
     FROM
       movie
+      order by movie_id
     
       `;
-  const movie = await db.get(getMovieQuery);
+  const movie = await db.all(getMovieQuery);
   response.send(movie);
+});
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  console.log(movieId);
+  const getoneMovieQuery = `
+    SELECT
+      movie_name
+    FROM
+      movie
+    WHERE 
+      movie_id=${movieId};
+    
+      `;
+  const moviel = await db.get(getoneMovieQuery);
+  console.log(getoneMovieQuery);
+  console.log(moviel);
+  response.send(moviel);
 });
 
 app.post("/movies/", async (request, response) => {
@@ -44,16 +63,34 @@ app.post("/movies/", async (request, response) => {
   const { directorId, movieName, leadActor } = movieDetails;
   const addmovieQuery = `
     INSERT INTO
-      movie (directorId, movieName,leadActor)
+      movie (director_id, movie_name,lead_actor)
     VALUES
       (
 
          ${directorId},
         '${movieName}',
-        '${leadActor}',
+        '${leadActor}'
       );`;
 
   const dbResponse = await db.run(addmovieQuery);
   const bookId = dbResponse.lastID;
-  response.send({ bookId: bookId });
+  response.send({ movieId: bookId });
+});
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const movieDetails = request.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+  const updateMovieQuery = `
+    UPDATE
+      movie
+    SET
+      director_id='${directorId}',
+      movie_name='${movieName}',
+      lead_actor='${leadActor}'
+    WHERE
+      movie_id = ${movieId}
+      `;
+  await db.run(updateMovieQuery);
+  response.send("Book Updated Successfully");
 });
